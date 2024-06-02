99b21192f4eb66228519ede0cb172ec553a1af26
Akhil <devarapalli148@gmail.com>
2024-06-02 10:00:53 -0400

    Spring project initlized

---
+++ app-api/app/src/main/java/com/lenders/app/LendersApplication.java
@@ -0,0 +1,13 @@
+package com.lenders.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendersApplication.class, args);
+	}
+
+}
---
+++ app-api/app/src/test/java/com/lenders/app/LendersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenders.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
