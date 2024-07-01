92e384c40270b8eabcae7e176fb313e43f414d7d
Matthew Morrison <msm8275@rit.edu>
2024-07-01 11:12:54 -0400

    Added getters and setters, fixed string format

--- app-api/app/src/main/java/com/lenders/app/model/House.java
+++ app-api/app/src/main/java/com/lenders/app/model/House.java
@@ -2,7 +2,6 @@ package com.lenders.app.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.ArrayList;
 import java.util.logging.Logger;
 import java.time.LocalDate;
 
@@ -15,11 +14,12 @@ public class House {
 
     private static final Logger LOG = Logger.getLogger(House.class.getName());
 
-    //TODO fix
     static final String STRING_FORMAT = """
-    House: [id=%d, address=%s, zipcode=%d, city=%s, sqft
-    property_value=%.2f, money_raised=%.2f, asking_price=%.2f, tags=%s]
-    """;
+        House: [id=%d, address=%s, zipcode=%d, city=%s, sqft=%d,
+        closed_on=%b, closing_date=%s, loan_amount=%.2f, ltv_percent=%.2f,
+        rehab_cost=%.2f, rehab_loan=%.2f, rehab_overview=%s, turn_around_date=%s,
+        gross_rent_estimate=%.2f, condition=%s, exit_strategy=%s, interest=%.2f]
+        """;
 
     public enum ExitStrategy {
         FIX_AND_RENT,
@@ -48,6 +48,7 @@ public class House {
     @JsonProperty("gross_rent_estimate") private float gross_rent_estimate;
     @JsonProperty("condition") private Condition condition;
     @JsonProperty("exit_strategy") private ExitStrategy exit_strategy;
+    @JsonProperty("interest") private float interest;
 
     /**
      * Create a new House Entity with the required JSON properties
@@ -83,7 +84,8 @@ public class House {
                  @JsonProperty("turn_around_date") LocalDate turn_around_date,
                  @JsonProperty("gross_rent_estimate") float gross_rent_estimate,
                  @JsonProperty("condition") Condition condition,
-                 @JsonProperty("exit_strategy") ExitStrategy exit_strategy) {
+                 @JsonProperty("exit_strategy") ExitStrategy exit_strategy,
+                 @JsonProperty("interest") float interest) {
         this.id = id;
         this.address = address;
         this.zipcode = zipcode;
@@ -100,44 +102,143 @@ public class House {
         this.gross_rent_estimate = gross_rent_estimate;
         this.condition = condition;
         this.exit_strategy = exit_strategy;
+        this.interest = interest;
     }
 
-    /**
-     * Get the house entity's id
-     * @return the house entity's id value
-     */
+    //Getters
+
     public int getId() {
         return this.id;
     }
 
-    /**
-     * Get the house entity's address
-     * @return the house entity's street address
-     */
     public String getAddress() {
         return this.address;
     }
 
-    /**
-     * Get the house entity's zipcode
-     * @return the house entity's zipcode
-     */
     public int getZipcode() {
         return this.zipcode;
     }
 
-    /**
-     * Get the house entity's city
-     * @return the house entity's city
-     */
     public String getCity() {
         return this.city;
     }
 
+    public int getSqft() {
+        return sqft;
+    }
+
+    public boolean isClosed_on() {
+        return closed_on;
+    }
+
+    public LocalDate getClosing_date() {
+        return closing_date;
+    }
+
+    public float getLoan_amount() {
+        return loan_amount;
+    }
+
+    public float getLtv_percent() {
+        return ltv_percent;
+    }
+
+    public float getRehab_cost() {
+        return rehab_cost;
+    }
+
+    public float getRehab_loan() {
+        return rehab_loan;
+    }
+
+    public String getRehab_overview() {
+        return rehab_overview;
+    }
+
+    public LocalDate getTurn_around_date() {
+        return turn_around_date;
+    }
+
+    public float getGross_rent_estimate() {
+        return gross_rent_estimate;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public ExitStrategy getExit_strategy() {
+        return exit_strategy;
+    }
+
+    public float getInterest() {
+        return interest;
+    }
+
+    // Setters
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setSqft(int sqft) {
+        this.sqft = sqft;
+    }
+
+    public void setClosed_on(boolean closed_on) {
+        this.closed_on = closed_on;
+    }
+
+    public void setClosing_date(LocalDate closing_date) {
+        this.closing_date = closing_date;
+    }
+
+    public void setLoan_amount(float loan_amount) {
+        this.loan_amount = loan_amount;
+    }
+
+    public void setLtv_percent(float ltv_percent) {
+        this.ltv_percent = ltv_percent;
+    }
+
+    public void setRehab_cost(float rehab_cost) {
+        this.rehab_cost = rehab_cost;
+    }
+
+    public void setRehab_loan(float rehab_loan) {
+        this.rehab_loan = rehab_loan;
+    }
+
+    public void setRehab_overview(String rehab_overview) {
+        this.rehab_overview = rehab_overview;
+    }
+
+    public void setTurn_around_date(LocalDate turn_around_date) {
+        this.turn_around_date = turn_around_date;
+    }
+
+    public void setGross_rent_estimate(float gross_rent_estimate) {
+        this.gross_rent_estimate = gross_rent_estimate;
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    public void setExit_strategy(ExitStrategy exit_strategy) {
+        this.exit_strategy = exit_strategy;
+    }
     
-
-
-
+    public void setInterest(float interest) {
+        this.interest = interest;
+    }
 
     /**
      * {@inheritDoc}
